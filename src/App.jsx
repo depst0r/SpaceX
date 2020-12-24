391767633340227e8b8f51b74424387481ed82aa
depst0r <depstor90@gmail.com>
2020-12-24 18:46:08 +0300

    'main'

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import { Header } from './Components/Header/Header';
-import logo from './logo.svg'
+import { Main } from './Components/Main/Main'
 import './style.css'
 
 function App() {
   return (
     <>
     <Header/>
-
-    <section className="main">
-    <h1 className="title">
-      Falcon 1
-    </h1>
-    <div className="video-container">
-      <video className="video" autoPlay loop muted src="./video/moon.mp4"></video>
-    </div>
-    </section>
+    <Main/>
 </>
   )
 }
