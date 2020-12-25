8975bb0d03273fedda9c418c68d0b6d771b7ff52
depst0r <depstor90@gmail.com>
2020-12-25 16:22:46 +0300

    '25.12'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Header } from './Components/Header/Header';
+import { Header } from './Components/Header/Header'
 import { Main } from './Components/Main/Main'
 import { Features } from './Components/Features/Features'
+import { Footer } from './Components/Footer/Footer'
 import './style.scss'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Header/>
     <Main/>
     <Features/>
+    <Footer/>
 </>
   )
 }
